3da39f0198a45f5b99d722b9d017bc7f89a62b06
otsetna <otsetna@rei.com>
2022-03-28 13:49:27 -0700

    SpringBoot_Project

--- Sprint-06-SprintBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ Sprint-06-SprintBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,12 +2,27 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
 
 @SpringBootApplication
 public class CydeoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CydeoApplication.class, args);
+
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = SpringApplication.run(CydeoApplication.class, args);
+
+        CommentService cs = context.getBean(CommentService.class);
+
+        cs.publishComment(comment);
     }
 
 }
+
