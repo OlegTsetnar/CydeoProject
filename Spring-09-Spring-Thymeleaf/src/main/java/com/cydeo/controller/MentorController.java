00181ef04be1931834014c0a79d76c534ea9b738
otsetna <otsetna@rei.com>
2022-04-03 16:10:18 -0700

    SpringBoot_Project-MVC_added HTML additional files

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cydeo.model.Mentor;
@@ -17,9 +19,16 @@ public class MentorController {
     @GetMapping("register")
     public String showForm(Model model) {
         model.addAttribute("mentor", new Mentor());
-        List<String> batchList = Arrays.asList("JD1","JD2","EU1","EU2","B18","B20");
-        model.addAttribute("batchList",batchList);
+        List<String> batchList = Arrays.asList("JD1", "JD2", "EU1", "EU2", "B18", "B20");
+        model.addAttribute("batchList", batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor) {
+
+               return "mentor/mentor-confirmation";
+       // return "redirect:/mentor/register";
     }
 }
