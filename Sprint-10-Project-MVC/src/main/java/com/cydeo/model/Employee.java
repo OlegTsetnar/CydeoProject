694bdaf24d0af926863cd7de19fbd887c9d39d74
otsetna <otsetna@rei.com>
2022-04-04 19:26:59 -0700

    SpringBoot_Project-MVC_Employee-Project

---
+++ Sprint-10-Project-MVC/src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,43 @@
+package com.cydeo.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Employee {
+
+
+    //    @NotNull            // Except null, anything can be accepted
+    //    @NotEmpty           // Except null and empty String anything can be accepted
+    //    @NotBlank           // Except null, empty String and only space anything can be accepted.
+
+    @NotBlank   // Oleg
+    @Size(max = 12, min = 2)
+    private String firstName;
+    private String lastName;
+
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
+
